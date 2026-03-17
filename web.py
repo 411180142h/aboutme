c9f8745134a1715fb10cd7ee2515b2a951893831
411180142h <hankfgh1230@gmail.com>
2026-03-17 11:49:35 +0800

    week4

---
+++ web.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "歡迎來到簡宏宥的網站首頁"
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    year = str(now.year)    #取得年份
+    month = str(now.month)  #取得月份
+    day = str(now.day)      #取得日期
+    now = year + "年" + month + "月" + day + "日"
+    return render_template("today.html", datetime = str(now))
+
+@app.route("/about")
+def about():
+    return render_template("mis2a.html")
+if __name__ == "__main__":
+    app.run(debug=True)
